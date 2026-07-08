28d6825fe269b42cdac3dfaae414067577907f2a
GabrielBlanc0o <gtbvacation@outlook.es>
2026-07-08 16:56:46 -0500

    feat: add Mascota entity with attributes and initial development diary

---
+++ src/main/java/com/gabrielblanco/veterinaria/model/Mascota.java
@@ -0,0 +1,18 @@
+package com.gabrielblanco.veterinaria.model;
+
+import java.time.LocalDate;
+
+public class Mascota {
+    private String nombre;
+
+    private LocalDate fechaNacimiento;
+
+    private String especie;
+
+    private String raza;
+
+    private Double peso;
+    
+    private String sexo;
+
+}
