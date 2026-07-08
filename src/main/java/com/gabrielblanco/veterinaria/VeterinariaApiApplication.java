a85f5fc21a91dd7aad7ea4d9b963c1fc2e6d035b
GabrielBlanc0o <gtbvacation@outlook.es>
2026-07-08 15:21:04 -0500

    style: format main method for better readability

--- src/main/java/com/gabrielblanco/veterinaria/VeterinariaApiApplication.java
+++ src/main/java/com/gabrielblanco/veterinaria/VeterinariaApiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class VeterinariaApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VeterinariaApiApplication.class, args);
+		SpringApplication.run(
+			VeterinariaApiApplication.class,
+			args
+		);
 	}
-
 }
