548f1dd4013feea9868512417f83a50b17901cfc
GabrielBlanc0o <gtbvacation@outlook.es>
2026-07-13 16:53:44 -0500

    feat: refactor MascotaService to improve validation and retrieval methods

--- src/main/java/com/gabrielblanco/veterinaria/service/MascotaService.java
+++ src/main/java/com/gabrielblanco/veterinaria/service/MascotaService.java
@@ -8,18 +8,59 @@ public class MascotaService {
     public MascotaService(MascotaRepository repository) {
         this.repository = repository;
     }
-    
-    public void registrarMascota(Mascota mascota) {
+
+    private Mascota obtenerMascota(String id) {
+        return repository.buscarPorId(id);
+    }
+
+    private void validarId(String id) {
+        if (id == null || id.isBlank()) {
+            throw new IllegalArgumentException(
+                "El id no puede estar vacío."
+            );
+        }
+    }
+
+    private void validarMascota(Mascota mascota) {
         if (mascota == null) {
             throw new IllegalArgumentException(
                 "La mascota no puede ser null."
             );
+        } else {
+            System.out.println("La mascota con id " + mascota.getId() + " si existe.");
         }
-        var res = repository.buscarPorId(mascota.getId());
-        if (res != null) {
-            throw new IllegalArgumentException("El id ya existe para esta Mascota.");
-        }
+    }
+    public Mascota buscarMascotaPorId(String id) {
+
+        validarId(id);
+        Mascota mascota = obtenerMascota(id);
+        validarMascota(mascota);
+        return mascota;
+    }
+    
+    public void registrarMascota(Mascota mascota) {
+        
+        Mascota verificar = obtenerMascota(mascota.getId());
+        validarMascota(verificar);
         repository.guardar(mascota);
     }
 
+    public void actualizarMascota(Mascota mascota) {
+        validarMascota(mascota);
+        Mascota verificar = obtenerMascota(mascota.getId());
+        if (verificar != null) {
+            repository.actualizar(mascota);
+        } else {
+            throw new IllegalArgumentException(
+                "No existe una mascota con el id: " + mascota.getId()
+            );
+        }
+    }
+
+    public void eliminarMascota(String id) {
+        validarId(id);
+        Mascota verificar = obtenerMascota(id);
+        validarMascota(verificar);
+        repository.eliminar(id);
+    }
 }
